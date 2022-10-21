6b80e8dddfe0c8dd6e13256761646a05c023b9f6
Dennis <abowlofmilchreis@gmail.com>
2022-10-21 08:51:42 +0200

    aufgabe 3.3

---
+++ Labor 3/aufgabe3.3/Debug/main.d
@@ -0,0 +1,31 @@
+main.o main.d main.o: ../main.c \
+ C:/Users/Dennis/NextCloud/desktop/Neuer\ Ordner/aufgabe3.3/Libraries/XMCLib/inc/xmc_common.h \
+ C:/Users/Dennis/NextCloud/desktop/Neuer\ Ordner/aufgabe3.3/Libraries/XMCLib/inc/xmc_device.h \
+ C:/Users/Dennis/NextCloud/desktop/Neuer\ Ordner/aufgabe3.3/Libraries/CMSIS/Infineon/XMC4500_series/Include/XMC4500.h \
+ C:/Users/Dennis/NextCloud/desktop/Neuer\ Ordner/aufgabe3.3/Libraries/CMSIS/Include/core_cm4.h \
+ C:/Users/Dennis/NextCloud/desktop/Neuer\ Ordner/aufgabe3.3/Libraries/CMSIS/Include/cmsis_version.h \
+ C:/Users/Dennis/NextCloud/desktop/Neuer\ Ordner/aufgabe3.3/Libraries/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/Dennis/NextCloud/desktop/Neuer\ Ordner/aufgabe3.3/Libraries/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Dennis/NextCloud/desktop/Neuer\ Ordner/aufgabe3.3/Libraries/CMSIS/Include/mpu_armv7.h \
+ C:/Users/Dennis/NextCloud/desktop/Neuer\ Ordner/aufgabe3.3/Libraries/CMSIS/Infineon/XMC4500_series/Include/system_XMC4500.h \
+ C:/Users/Dennis/NextCloud/desktop/Neuer\ Ordner/aufgabe3.3/Libraries/xmc4500_lib.h
+
+C:/Users/Dennis/NextCloud/desktop/Neuer\ Ordner/aufgabe3.3/Libraries/XMCLib/inc/xmc_common.h:
+
+C:/Users/Dennis/NextCloud/desktop/Neuer\ Ordner/aufgabe3.3/Libraries/XMCLib/inc/xmc_device.h:
+
+C:/Users/Dennis/NextCloud/desktop/Neuer\ Ordner/aufgabe3.3/Libraries/CMSIS/Infineon/XMC4500_series/Include/XMC4500.h:
+
+C:/Users/Dennis/NextCloud/desktop/Neuer\ Ordner/aufgabe3.3/Libraries/CMSIS/Include/core_cm4.h:
+
+C:/Users/Dennis/NextCloud/desktop/Neuer\ Ordner/aufgabe3.3/Libraries/CMSIS/Include/cmsis_version.h:
+
+C:/Users/Dennis/NextCloud/desktop/Neuer\ Ordner/aufgabe3.3/Libraries/CMSIS/Include/cmsis_compiler.h:
+
+C:/Users/Dennis/NextCloud/desktop/Neuer\ Ordner/aufgabe3.3/Libraries/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/Dennis/NextCloud/desktop/Neuer\ Ordner/aufgabe3.3/Libraries/CMSIS/Include/mpu_armv7.h:
+
+C:/Users/Dennis/NextCloud/desktop/Neuer\ Ordner/aufgabe3.3/Libraries/CMSIS/Infineon/XMC4500_series/Include/system_XMC4500.h:
+
+C:/Users/Dennis/NextCloud/desktop/Neuer\ Ordner/aufgabe3.3/Libraries/xmc4500_lib.h:
